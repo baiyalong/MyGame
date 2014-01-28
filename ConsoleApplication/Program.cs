66a4d846d5dca041858cd229665966403bcadc4d
baiyalong <baiyalong@live.cn>
2014-01-28 16:48:10 +0800

    1

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -8,18 +8,18 @@ using System.Configuration;
 using _00Common;
 using _01DAL;
 using log4net;
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace ConsoleApplication
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+    (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
-            Map m = Map.Instance;
-            Node c = new Node();
 
-            Console.WriteLine(c.ToString());
-            
+            log.Info("Info logging");
 
             Console.Read();
         }
